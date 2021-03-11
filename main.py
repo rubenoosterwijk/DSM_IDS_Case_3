0f510d1e17973c473bd69fb2aee261d75d71b64b
Ruben <ruben.oosterwijk@hva.nl>
2021-03-11 16:12:10 +0100

    Imported laadpaaldata

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import streamlit
 import pandas as pd
 import numpy as np
-
-
-
+import os
 
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# imported the given data
+laadpaaldata = pd.read_csv("laadpaaldata.csv")
+laadpaaldata.head()
+
+# Potentiële informatie
+# Hoe ziet een gemiddeld bezetting van een laadpaal er uit?
+# Wat is het verschil tussen laden en bezetten van een laadpaal?
+# Hoe ziet het gemiddelde laadprofiel er uit?
+# Wat is de verdeling in vermogens?
+# ….
 
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
