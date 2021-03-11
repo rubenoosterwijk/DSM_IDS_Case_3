8e7999415291f44e696bd2194000c50b9af3fe45
rbaarspul <rubenbaarspul@hotmail.com>
2021-03-11 18:44:05 +0100

    Titels, foto en potienteel interessante data in punten gezet

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Onderzoek laadpalen')
+st.title('Onderzoek naar laadpalen')
 
 laadpaaldata = pd.read_csv("laadpaaldata.csv")
 laadpaaldata["OverChargeTime"] = laadpaaldata["ConnectedTime"]-laadpaaldata["ChargeTime"]
 print(laadpaaldata.head())
 
-st.button('Hit me')
+
 st.image('laadpaalafbeelding.jpeg')
+st.header('De data die wij onderzocht hebben:')
+
+st.markdown("* Bezetting laadpalen")
+st.markdown("* Wat is het verschil tussen laden en bezetten van een laadpaal?")
+st.markdown("* Hoe ziet het gemiddelde laadprofiel er uit?")
+st.markdown("* Wat is de verdeling in vermogens? Lekker losjes")
 st.dataframe(laadpaaldata)
 st.line_chart(laadpaaldata)
 
-st.text("""
-#Potentiële informatie
-Hoe ziet een gemiddeld bezetting van een laadpaal er uit?
-Wat is het verschil tussen laden en bezetten van een laadpaal?
-Hoe ziet het gemiddelde laadprofiel er uit?
-Wat is de verdeling in vermogens? Lekker losjes
-"""
-)
+
+
+
