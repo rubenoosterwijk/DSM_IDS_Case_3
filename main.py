3be686dab32c679dfb60fa1e21171e599fbbd7f0
Ruben <ruben.oosterwijk@hva.nl>
2021-03-11 16:53:03 +0100

    Added OverChargeTime Column om de overbezetting te weergeven

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
 laadpaaldata = pd.read_csv("laadpaaldata.csv")
-print(laadpaaldata.columns)
+laadpaaldata["OverChargeTime"] = laadpaaldata["ConnectedTime"]-laadpaaldata["ChargeTime"]
+print(laadpaaldata.head())
 
 
 st.text("""
-
 #Potentiële informatie
 Hoe ziet een gemiddeld bezetting van een laadpaal er uit?
 Wat is het verschil tussen laden en bezetten van een laadpaal?
 Hoe ziet het gemiddelde laadprofiel er uit?
 Wat is de verdeling in vermogens?
-….
-
 """
 )
