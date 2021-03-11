5e8641517479780224956d4b24e9ec3401ed764e
Ruben <ruben.oosterwijk@hva.nl>
2021-03-11 16:32:52 +0100

    Added stuff

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-import streamlit
+import streamlit as st
 import pandas as pd
 import numpy as np
 
+laadpaaldata = pd.read_csv("laadpaaldata.csv")
+print(laadpaaldata.columns)
 
 
+st.text("""
 
-# This is a sample Python script.
+#Potentiële informatie
+Hoe ziet een gemiddeld bezetting van een laadpaal er uit?
+Wat is het verschil tussen laden en bezetten van een laadpaal?
+Hoe ziet het gemiddelde laadprofiel er uit?
+Wat is de verdeling in vermogens?
+….
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"""
+)
