280a5c2cb9c0a4f673740cba18f6e758cd0c27e4
rbaarspul <rubenbaarspul@hotmail.com>
2021-03-11 17:15:38 +0100

    Onderling takenverdeeld

--- OpenChargeMap.py
+++ OpenChargeMap.py
@@ -6,3 +6,4 @@ import seaborn as sns
 #Bud De data van de locaties van de laadpalen verstrekken. Onthoud je gaat met de map werken en je moet dus een maar weergeven op streamlit.
 #Kijk alvast naar voorbeelden op de site of we met de map exrtra interactieve dingen kunnen doen (widget gebruik)
 
+#Onderling even uitmaken wie wat gaat onderzoeken
