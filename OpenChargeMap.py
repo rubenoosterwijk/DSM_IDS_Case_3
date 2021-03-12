911bba5b4c17925fa6c96e4835fa33b46ec86342
scurvydawg <buddewit@gmail.com>
2021-03-12 10:59:11 +0100

    API Data Nederland

--- OpenChargeMap.py
+++ OpenChargeMap.py
@@ -3,11 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
-response = requests.get("https://api.openchargemap.io/v3/referencedata/&key=f7903c6a-d2cd-4a10-b012-260c3536974f")
+response = requests.get("https://api.openchargemap.io/v3/poi/?output=json&countrycode=NL&key=f7903c6a-d2cd-4a10-b012-260c3536974f")
 print(response.status_code)
 print(response.text)
 response.json()
-
-response2 = requests.get(response.json()['Countries'])
-print(response2.status_code)
-response2.json() # dit is een Python dictionary
