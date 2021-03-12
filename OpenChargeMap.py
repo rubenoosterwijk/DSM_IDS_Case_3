629612d1e6817af8a11d6f1b050bd13413ae580e
scurvydawg <buddewit@gmail.com>
2021-03-12 10:39:12 +0100

    API Data opgehaald

--- OpenChargeMap.py
+++ OpenChargeMap.py
@@ -3,9 +3,11 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
-response = requests.get("https://api.openchargemap.io/v3/referencedata/")
+response = requests.get("https://api.openchargemap.io/v3/referencedata/&key=f7903c6a-d2cd-4a10-b012-260c3536974f")
 print(response.status_code)
 print(response.text)
 response.json()
 
-key=f7903c6a-d2cd-4a10-b012-260c3536974f
+response2 = requests.get(response.json()['Countries'])
+print(response2.status_code)
+response2.json() # dit is een Python dictionary
