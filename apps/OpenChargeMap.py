c3152eb08b554e5ee0b02c15e6fc4ce4bb32186a
scurvydawg <buddewit@gmail.com>
2021-03-12 12:57:53 +0100

    API Data Nederland 2

--- apps/OpenChargeMap.py
+++ apps/OpenChargeMap.py
@@ -10,16 +10,14 @@ import requests
 #Onderling even uitmaken wie wat gaat onderzoeken
 
 
-response = requests.get("https://api.openchargemap.io/v3/poi/?output=csv&countrycode=NL&key=f7903c6a-d2cd-4a10-b012-260c3536974f")
-print(response.status_code)
-print(response.text)
-
-import streamlit as st
-
 # Alles wat je runt per pagina moet in de def app(): komen. Anders runt hij de pagina niet.
 def app():
-    st.title('Home')
+    response = requests.get(
+        "https://api.openchargemap.io/v3/poi/?output=json&countrycode=NL&key=f7903c6a-d2cd-4a10-b012-260c3536974f")
+    json = response.text
+    df = pd.read_json(json)
 
-    st.write('This is the `home page` of this multi-page app.')
+    print(response.status_code)
+    print(df.head())
 
-    st.write('In this app, we will be building a simple classification model using the Iris dataset.')
+app()
