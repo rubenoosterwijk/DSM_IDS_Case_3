0bc594bdbf7a25cedb3b5723499a2014419a6cb5
Ruben <ruben.oosterwijk@hva.nl>
2021-03-15 16:47:24 +0100

    Begin gemaakt aan laadpaallocaties.py en in het jupyter notebook bestand

--- apps/OpenChargeMap.py
+++ apps/OpenChargeMap.py
@@ -15,13 +15,14 @@ import json as js
 # Alles wat je runt per pagina moet in de def app(): komen. Anders runt hij de pagina niet.
 def app():
 
-    laadpaaldata = pd.read_csv('laadpaallocaties.csv')
-    st.text(laadpaaldata.info())
+    openchargemap = pd.read_csv('laadpaallocaties.csv')
 
-    st.text(laadpaaldata.describe())
+    st.text(openchargemap.info())
+
+    st.text(openchargemap.describe())
 
     # openchargemap["OverChargeTime"] = openchargemap["ConnectedTime"] - openchargemap["ChargeTime"]
-    print(laadpaaldata.head())
+    print(openchargemap.head())
 
     st.image('laadpaalafbeelding.jpeg')
     st.header('De data die wij onderzocht hebben:')
@@ -30,5 +31,5 @@ def app():
     st.markdown("* Wat is het verschil tussen laden en bezetten van een laadpaal?")
     st.markdown("* Hoe ziet het gemiddelde laadprofiel er uit?")
     st.markdown("* Wat is de verdeling in vermogens?")
-    st.dataframe(laadpaaldata)
-    st.line_chart(laadpaaldata)
+    st.dataframe(openchargemap)
+    st.line_chart(openchargemap)
