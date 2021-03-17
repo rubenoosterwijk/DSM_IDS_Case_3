889fe557c23b37150fac5def1b92121e6bebb20e
rbaarspul <rubenbaarspul@hotmail.com>
2021-03-17 23:08:23 +0100

    Tekst aangepast

--- apps/OpenChargeMap.py
+++ apps/OpenChargeMap.py
@@ -245,7 +245,7 @@ def app():
     st.markdown("* Hoeveel laadpalen zijn er in de loop van de jaren bijgekomen?")
     st.markdown("* Waar staan de laadpalen in Nederland en per stad?")
 
-    print(sns.histplot(data=openchargemap,
+     print(sns.histplot(data=openchargemap,
                  x="ChargerType",
                  shrink=.2,
                  hue="ChargerType"))
