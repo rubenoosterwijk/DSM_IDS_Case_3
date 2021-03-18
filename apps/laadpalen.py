00b67124bea14dcf8b37684de0c240d7dd9a10c2
Ruben <ruben.oosterwijk@hva.nl>
2021-03-18 19:57:20 +0100

    Laadpalen afgemaakt

--- apps/laadpalen.py
+++ apps/laadpalen.py
@@ -12,10 +12,6 @@ def header():
     st.title('Onderzoek naar laadpalen: Deel 2')
     st.image('laadpaalafbeelding.jpeg')
     st.header('Data visualisatie:')
-    st.markdown("* Bezetting laadpalen")
-    st.markdown("* Wat is het verschil tussen laden en bezetten van een laadpaal?")
-    st.markdown("* Hoe ziet het gemiddelde laadprofiel er uit?")
-    st.markdown("* Wat is de verdeling in vermogens?")
 
 
 def main():
@@ -26,56 +22,70 @@ def main():
     laadpaaldata['Ended'] = pd.to_datetime(laadpaaldata['Ended'], format='%Y-%m-%d %H:%M:%S')
 
     # st.write(laadpaaldata.describe())
+    #------------------------------------------------------------------------------------------------------
 
     colomns = ["TotalEnergy", "ConnectedTime", "ChargeTime", "MaxPower", "OverCharged", "Weekday"]
 
+    st.subheader("Wat is de verdeling in vermogens?")
+
+    st.text("Hieronder kunt you de histogram hieronder aanpassen."
+            "")
     option2 = st.selectbox(
         'Selecteer uw column voor de histogram?',
         (colomns))
-    st.write('You selected:', option2)
 
-    if option2=="Weekday":
+    if option2 == "Weekday":
 
         week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-        optionsdict = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6}
+        optionsdict = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday': 5,
+                       'Sunday': 6}
 
         week1, week2 = st.select_slider(
-            'Select a range for dates created',
+            'Selecteer een speciafieke weekdag (0=Maandag)',
             options=week,
             value=('Monday', 'Sunday'))
 
         week3 = optionsdict[week1]
         week4 = optionsdict[week2]
 
-        st.write(week3, week4)
+        # st.write(week3, week4)
 
-        st.dataframe(laadpaaldata)
+        # st.dataframe(laadpaaldata)
         df2 = laadpaaldata.loc[
             ((laadpaaldata['Weekday'] >= week3) & (laadpaaldata['Weekday'] <= week4))]
         # st.dataframe(df2)
 
-        sns.histplot(data=df2, x=option2)
+        st.subheader("Histogram", option2)
+        sns.histplot(data=df2, x=option2, bins="auto")
         st.pyplot()
     else:
         df2 = laadpaaldata
-        sns.histplot(data=df2, x=option2)
+        st.subheader("Histogram")
+        sns.histplot(data=df2, x=option2, bins="auto")
         st.pyplot()
 
 
+    #------------------------------------------------------------------------------------------------------
 
-    # options = st.multiselect(
-    #     'Selecteer de weekdagen', ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-    #     ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+    st.subheader(
+        "Een histogram van de laadtijd met de bijhorende boxplot.\n"
+        "Dit in combinatie meteen annotatie van het gemiddelde en de median en een benadering van de kansdichtheidsfunctie."
+        "")
 
-    # st.dataframe(df2)
+    sns.histplot(data=df2, x="ChargeTime", bins="auto", kde=True)
 
-    sns.histplot(data=df2, x="ChargeTime", bins=30, kde=True, element="step")
-
-    plt.axvline(x=laadpaaldata.ChargeTime.mean(), linewidth=1, color='g', label="mean", alpha=0.5)
-    plt.axvline(x=laadpaaldata.ChargeTime.median(), linewidth=1, color='y', label="median", alpha=0.5)
+    plt.axvline(x=laadpaaldata.ChargeTime.mean(), linewidth=1, color='r', label="mean", alpha=0.5)
+    plt.axvline(x=laadpaaldata.ChargeTime.median(), linewidth=1, color='g', label="median", alpha=0.5)
 
     plt.legend(["mean", "median"])
     st.pyplot()
+
+    sns.boxplot(data=df2, x="ChargeTime")
+    st.pyplot()
+
+
+    #------------------------------------------------------------------------------------------------------
+
 
     sns.histplot(data=laadpaaldata, x="ChargeTime", y="ConnectedTime", bins=40, cbar=True, cbar_kws=dict(shrink=.75))
 
@@ -84,9 +94,10 @@ def main():
 
     plt.legend(["mean", "median"])
     st.pyplot()
-
-
-
+    #------------------------------------------------------------------------------------------------------
+    st.subheader(
+        "Een scatterplot over tijd.\n"
+        "Gebruik de tijdslider en de dropdown menu om een column te selecteren")
     colomns = ["TotalEnergy", "ConnectedTime", "ChargeTime", "MaxPower"]
     col_one_list = laadpaaldata['Started'].tolist()
 
@@ -97,7 +108,6 @@ def main():
         'Selecteer uw column voor de y-as',
         (colomns))
     st.write('You selected:', option3)
-
 
     start_slider, end_slider = st.select_slider(
         'Select a range for dates created',
@@ -112,6 +122,7 @@ def main():
     plt.xticks(rotation=45)
 
     st.pyplot()
+    #------------------------------------------------------------------------------------------------------
 
 
 # Alles wat je runt per pagina moet in de def app(): komen. Anders runt hij de pagina niet.
@@ -120,3 +131,5 @@ def app():
     header()
     main()
 
+
+app()
